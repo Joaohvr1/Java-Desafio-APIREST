a3a1081ce3e73ab86b1618d0b71dda87ee7c8108
João Henrique <joaohenriquevidalribeiro@gmail.com>
2023-10-21 19:48:00 -0300

    Start Project

---
+++ src/test/java/me/dio/restapi/SantanderDevFullstackAngularJava2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevFullstackAngularJava2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
